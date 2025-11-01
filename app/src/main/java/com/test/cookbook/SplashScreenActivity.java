5c99d362b78a573106619b88c5e4deb74f258c59
anirudhchakraborty <preetiujjal@gmail.com>
2025-11-01 20:07:44 +0530

    feat(splash): add background and logo; fix splash screen duration

--- app/src/main/java/com/test/cookbook/SplashScreenActivity.java
+++ app/src/main/java/com/test/cookbook/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package com.test.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.os.Looper;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +17,19 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.splash_screen);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Handler handler = new Handler(Looper.getMainLooper()); //(the main thread looper)
+        handler.postDelayed(() -> // postDelayed is the method used to delay, "->" this is the lambda expression
+        {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // finish so that if the user presses back, they don't return to the splash screen
+        }, 5000); // 2 seconds delay
     }
 }
